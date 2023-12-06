037d91716992f5c37b14530bd2ab76113ec7651c
nuengnueng <hw45498@horwang.ac.th>
2023-12-06 13:56:51 +0700

    finished

--- mycalculator.py
+++ mycalculator.py
@@ -4,44 +4,17 @@ class mycalculator:
     def __init__(self):
 
       self.root = tk.Tk()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       self.display = tk.StringVar()
-=======
-      self.label_text = tk.StringVar()
-      self.label_text.set("mycalculator")
->>>>>>> fea
-=======
-      self.label_text = tk.StringVar()
-      self.label_text.set("mycalculator")
->>>>>>> dev
 
       self.root.geometry ("300x370")
       self.root.title(mycalculator)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-      self.label = tk.Label(self.root, textvariable=self.display, font=('Arial', 18)) 
-=======
       self.label = tk.Label(self.root, textvariable=self.display, font=('Arial', 18))
->>>>>>> fea
-=======
-      self.label_text = tk.StringVar()
-      self.label_text.set("mycalculartor")
-  
-      self.root.geometry ("300x300")
-      self.root.title("mycalculator")
-
-      self.label = tk.Label(self.root, textvariable=self.display, font=('Arial', 18)) 
->>>>>>> origin/feature/add--
-=======
-      self.label = tk.Label(self.root, textvariable=self.display, font=('Arial', 18))
->>>>>>> dev
       self.label.pack()
 
       self.button = tk.Button(self.root, text="AC", height=3, width=7)
       self.button.place(x=20, y=50)
+      self.button.bind("<Button-1>", self.action_06)
       self.button = tk.Button(self.root, text="7", height=3, width=7)
       self.button.place(x=20, y=110)
       self.button.bind("<Button-1>", self.action_7)
@@ -56,6 +29,7 @@ class mycalculator:
       self.button.bind("<Button-1>", self.action_0)
       self.button = tk.Button(self.root, text="+/-", height=3, width=7)
       self.button.place(x=85, y=50)
+      self.button.bind("<Button-1>", self.action_07)
       self.button = tk.Button(self.root, text="8", height=3, width=7)
       self.button.place(x=85, y=110)
       self.button.bind("<Button-1>", self.action_8)
@@ -67,6 +41,7 @@ class mycalculator:
       self.button.bind("<Button-1>", self.action_2)
       self.button = tk.Button(self.root, text="%", height=3, width=7)
       self.button.place(x=150, y=50)
+      self.button.bind("<Button-1>", self.action_08)
       self.button = tk.Button(self.root, text="9", height=3, width=7)
       self.button.place(x=150, y=110)
       self.button.bind("<Button-1>", self.action_9)
@@ -78,26 +53,19 @@ class mycalculator:
       self.button.bind("<Button-1>", self.action_3)
       self.button = tk.Button(self.root, text=".", height=3, width=7)
       self.button.place(x=150, y=290)
+      self.button.bind("<Button-1>", self.action_05)
       self.button = tk.Button(self.root, text="/", height=3, width=7)
-<<<<<<< HEAD
       self.button.place(x=215, y=50)
+      self.button.bind("<Button-1>", self.action_04)
       self.button = tk.Button(self.root, text="*", height=3, width=7)
       self.button.place(x=215, y=110)
       self.button.bind("<Button-1>", self.action_03)
-=======
-      self.button.bind('<Button-1>', self.action_04)
-
-      self.button.place(x=215, y=50)
-      self.button = tk.Button(self.root, text="*", height=3, width=7)
-      self.button.place(x=215, y=110)
->>>>>>> origin/feature/add--
       self.button = tk.Button(self.root, text="-", height=3, width=7)
       self.button.place(x=215, y=170)
+      self.button.bind("<Button-1>", self.action_02)
       self.button = tk.Button(self.root, text="+", height=3, width=7)
       self.button.place(x=215, y=230)
-<<<<<<< HEAD
       self.button.bind('<Button-1>',self.action_01)
-      
       self.button = tk.Button(self.root, text="=", height=3, width=7)
       self.button.place(x=215, y=290)
       self.button.bind("<Button-1>", self.action_00)
@@ -105,7 +73,7 @@ class mycalculator:
 
       self.root.mainloop()
 
-<<<<<<< HEAD
+
     def action_03(self, event):
       self.display.set("*")
       print(event)
@@ -143,28 +111,26 @@ class mycalculator:
     def action_9(self, event):
       self.display.set("9")
       print(event)
-      
-
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/add
-=======
     def action_01(self, event):
-       self.display.self("+")
-       print(event)
-       
-
->>>>>>> fea
-=======
-      self.button = tk.Button(self.root, text="=", height=3, width=7)
-      self.button.place(x=215, y=290)
-      self.root.mainloop()
-
+      self.display.self("+")
+      print(event)
     def action_04(self, event):
-       self.display.set("/")
-       print(event)
+      self.display.set("/")
+      print(event)
+    def action_02(self, event):
+      self.display.set("-")
+      print(event)
+    def action_05(self, event):
+      self.display.set(".")
+      print(event)
+    def action_06(self, event):
+      self.display.set("0")
+      print(event)
+    def action_07(self, event):
+      self.display.set("+/-")
+      print(event)
+    def action_08(self, event):
+      self.display.set("%")
+      print(event)
      
-
->>>>>>> origin/feature/add--
 mycalculator()
