f600aa871a9ad54cfeb32a7800a9d263ddbb9b4b
tm20092207 <tml20092207@gmail.com>
2024-01-10 16:37:13 +0700

    times and equal

--- mycalculator.py
+++ mycalculator.py
@@ -75,3 +75,17 @@ class mycalculator:
 
 
       self.root.mainloop()
+
+    def action_03(self, event):
+      self.display.set("*")
+      self.c()
+      self.operator = "*"
+      self.number_last = self.number_now
+      self.display.set(str(self.number_now))
+      self.number_now = 0
+      print(event)
+    
+    def action_00(self, event):
+      self.c()
+      self.display.set(str(self.number_now))
+      print(event)   
