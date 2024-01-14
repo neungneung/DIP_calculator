e25656c6bb1f341389b449ce005d9714746c9741
neungneung <hw45498@horwang.ac.th>
2024-01-14 21:33:15 +0700

    chang maeng

--- main.py
+++ main.py
@@ -146,13 +146,38 @@ class mycalculator:
       self.c()
       self.display.set(str(self.number_now))
       print(event)
+
+    def action_01(self, event):
+      self.display.set("+")
+      self.c()
+      self.operator = "+"
+      self.number_last = self.number_now
+      self.display.set(str(self.number_now))
+      self.number_now = 0
+      print(event)
     
-    
+    def action_02(self, event):
+      self.display.set("-")
+      self.c()
+      self.operator = "-"
+      self.number_last = self.number_now
+      self.display.set(str(self.number_now))
+      self.number_now = 0
+      print(event)
 
     def action_03(self, event):
       self.display.set("*")
       self.c()
       self.operator = "*"
+      self.number_last = self.number_now
+      self.display.set(str(self.number_now))
+      self.number_now = 0
+      print(event)
+
+    def action_04(self, event):
+      self.display.set("/")
+      self.c()
+      self.operator = "/"
       self.number_last = self.number_now
       self.display.set(str(self.number_now))
       self.number_now = 0
