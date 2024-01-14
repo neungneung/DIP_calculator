0d27c443f2c252bb7ee411bc74834c6a66d8c4fb
neungneung <hw45498@horwang.ac.th>
2024-01-14 21:37:43 +0700

    maitalok

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-def action_01(self, event):
-      self.c()
-      self.operator = "+"
-      self.number_last = self.number_now
-      self.display.set(str(self.number_now))
-      self.number_now = 0
-      print(event)
-
-    def action_02(self, event):
-      self.c()
-      self.operator = "-"
-      self.number_last = self.number_now
-      self.display.set(str(self.number_now))
-      self.number_now = 0
-      print(event)
+      self.operator = "/"
